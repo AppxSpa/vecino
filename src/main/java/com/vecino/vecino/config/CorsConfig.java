94d5ead3947f5c07bd52be24b68f681b86d55d3f
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2025-07-30 12:36:35 -0400

    cros config

--- src/main/java/com/vecino/vecino/config/CorsConfig.java
+++ src/main/java/com/vecino/vecino/config/CorsConfig.java
@@ -1,5 +1,4 @@
 package com.vecino.vecino.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,14 +10,14 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todas las rutas
-                        .allowedOrigins("http://localhost:5173") // Permitir origen específico
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                        .allowedHeaders("*") // Todos los encabezados permitidos
-                        .allowCredentials(true) // Permitir envío de cookies y encabezados de autenticación
-                        .maxAge(3600); // Tiempo de caché
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Acepta cualquier origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false); // No se usan cookies ni JWT
             }
         };
     }
