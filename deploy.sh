d396c9c81c52dedf154f0bf118d9d9a07d3d19c7
Mirko Gutierrez <mirko.gutierrez@appx.cl>
2026-01-21 18:28:17 -0300

    agrega dokerfile

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+sudo docker stop vecino-container 2>/dev/null
+sudo docker rm vecimp-container 2>/dev/null
+
+sudo docker build -t vecino .
+
+sudo docker run \
+           --restart always \
+           -d -p 8080:8080 \
+           --env-file .env \
+           --network appx \
+           --add-host=host.docker.internal:host-gateway \
+           --name vecino-container vecino
